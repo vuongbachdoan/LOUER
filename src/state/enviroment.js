7cf568ad1aac12419aebb8ff264b8372e381fc25
Dat Hoang <dathtd119@gmail.com>
2023-10-24 21:14:08 +0700

    config for eas apk

--- src/state/enviroment.js
+++ src/state/enviroment.js
@@ -3,4 +3,5 @@ import { Store } from 'pullstate';
 export const enviroment = new Store({
     value: true,
     clerkPublicKey : 'pk_test_Zmx1ZW50LXNlYWhvcnNlLTQuY2xlcmsuYWNjb3VudHMuZGV2JA',
+    clerkPublicKey_LIVE : 'pk_live_Y2xlcmsubG91ZXJhcHAuY29tJA'
 });
